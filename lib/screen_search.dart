971827676e50015e034b309bb0b660551fe69050
artemleusenko <artem20072003@outlook.com>
2022-12-01 10:47:50 +0300

    update

---
+++ lib/screen_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ScreenC extends StatelessWidget {
+  const ScreenC({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('О приложении'),
+      ),
+      body: Center(),
+    );
+  }
+}
